2fadc71e5591fc2781415b00c8e1d88c4c604a41
KonstantinSergienko <50620992+KonstantinSergienko@users.noreply.github.com>
2020-08-01 16:15:41 +0500

    create header component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 
+import Header from "./components/Header";
 class App extends React.Component {
   render() {
-    return <div>Hello</div>;
+    return (
+      <React.Fragment>
+        <Header />
+      </React.Fragment>
+    );
   }
 }
 
